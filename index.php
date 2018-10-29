2f602413a0c334f671f74bea957aad6c6bb26254
Jashim Uddin <jashim.ncn@gmail.com>
2018-10-30 02:01:33 +0600

    removed main design

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 // Initialize the session
-session_start();
 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-	header("location: login.php");
-	exit;
-}
+// session_start();
+
+// // Check if the user is logged in, if not then redirect him to login page
+// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+// 	header("location: login.php");
+// 	exit;
+// }
 ?>
 
 <!DOCTYPE html>
@@ -21,11 +22,13 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
 <div class="page-header">
-	<h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+	<div class="row">
+		<div class="card card-body bg-dark mx-auto">
+		<h1>Hi,</b>. Welcome to our site.</h1>
+		</div>
+	</div>
+	
 </div>
-<p>
-	<a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-	<a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-</p>
+
 </body>
 </html>
